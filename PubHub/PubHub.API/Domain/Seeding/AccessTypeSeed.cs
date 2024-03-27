d74cb3207243e0f9f805e91a0cd35d9f90127c43
ZhakalenDK <27011156+ZhakalenDk@users.noreply.github.com>
2024-03-27 09:55:34 +0100

    Added seeding for AccessType

---
+++ PubHub/PubHub.API/Domain/Seeding/AccessTypeSeed.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PubHub.API.Domain.Entities;
+
+namespace PubHub.API.Domain.Seeding
+{
+    public class AccessTypeSeed : IEntityTypeConfiguration<AccessType>
+    {
+        public void Configure(EntityTypeBuilder<AccessType> builder)
+        {
+            builder.HasData(
+                new AccessType { Id = 1, Name = "Owner" },
+                new AccessType { Id = 2, Name = "Subscriber" },
+                new AccessType { Id = 3, Name = "Borrower" },
+                new AccessType { Id = 4, Name = "Expired" });
+        }
+    }
+}
