21ade89f16278d6f2b5bbe2a274a77e87fa57f13
Sjimooon <122673993+Sjimooon@users.noreply.github.com>
2024-04-04 22:06:38 +0200

    Basic unit test setup.

---
+++ Test/PubHub.API.UT/PublishersControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using PubHub.API.Controllers;
+using PubHub.API.Domain.Entities;
+using PubHub.API.UT.Utilities;
+using PubHub.Common.Models.Publishers;
+
+namespace PubHub.API.UT
+{
+    public class PublishersControllerTests : ControllerTests
+    {
+        private readonly PublishersController _controller;
+
+        public PublishersControllerTests(DatabaseFixture databaseFixture)
+            : base(databaseFixture)
+        {
+            _controller = new(Substitute.For<ILogger<PublishersController>>(), _context);
+        }
+
+        [Fact]
+        public async Task GetAllPublishersAsync()
+        {
+            // Arrange.
+            // TODO (SIA): Simplify and extend test data creation.
+            Fixture fixture = new Fixture();
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            fixture.Customize<Publisher>(x => x
+                .Without(p => p.Books));
+
+            var publisher = fixture.Create<Publisher>();
+
+            await _context.Set<Publisher>().AddRangeAsync(publisher);
+            var query = new PublisherQuery()
+            {
+                OrderBy = OrderPublisherBy.Name,
+                Descending = true,
+                Max = 1,
+                Page = 1
+            };
+
+            // Act.
+            var result = await _controller.GetPublishersAsync(query);
+
+            // Assert.
+            var response = Assert.IsAssignableFrom<Ok<PublisherInfoModel[]>>(result);
+            var valueResult = Assert.IsAssignableFrom<IValueHttpResult<PublisherInfoModel[]>>(result);
+
+            // TODO (SIA): Assert value.
+        }
+    }
+}
---
+++ Test/PubHub.API.UT/Utilities/DatabaseFixture.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.MsSql;
+
+namespace PubHub.API.UT.Utilities
+{
+    public class DatabaseFixture : IAsyncDisposable
+    {
+        private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder().Build();
+        private readonly Task _containerStartTask;
+
+        private DbContextOptions<PubHubContext> _options = new();
+        private SqlConnection? _connection;
+
+        public DatabaseFixture()
+        {
+            _containerStartTask = _msSqlContainer.StartAsync();
+            CreateDatabaseAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task CreateDatabaseAsync()
+        {
+            // Wait until container is started.
+            await _containerStartTask;
+
+            // Connect to database container.
+            _connection ??= new(_msSqlContainer.GetConnectionString());
+            _connection.Open();
+
+            // Create options.
+            _options = new DbContextOptionsBuilder<PubHubContext>()
+                .UseSqlServer(_connection)
+                .Options;
+
+            // Build database schema.
+            using var context = GetDBContext();
+            context.Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Get database context to the current test database.
+        /// </summary>
+        /// <returns>A new <see cref="PubHubContext"/>.</returns>
+        public PubHubContext GetDBContext()
+        {
+            return new PubHubContext(_options);
+        }
+
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            GC.SuppressFinalize(this);
+            await _msSqlContainer.DisposeAsync();
+        }
+    }
+}
