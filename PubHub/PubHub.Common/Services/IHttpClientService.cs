efececef695a8f4b57a975d7dcc26b69ec982f92
Sjimooon <122673993+Sjimooon@users.noreply.github.com>
2024-04-04 09:43:21 +0200

    Non nullable.

--- PubHub/PubHub.Common/Services/IHttpClientService.cs
+++ PubHub/PubHub.Common/Services/IHttpClientService.cs
@@ -17,7 +17,7 @@
         /// <param name="uri">URI (must NOT start with '/').</param>
         /// <param name="httpContent">Content of the POST message.</param>
         /// <returns>HTTP response.</returns>
-        public Task<HttpResponseMessage?> PostAsync(string uri, HttpContent? httpContent = null);
+        public Task<HttpResponseMessage> PostAsync(string uri, HttpContent? httpContent = null);
 
         /// <summary>
         /// Send a PUT message using the base address of <see cref="Client"/>.
@@ -25,13 +25,13 @@
         /// <param name="uri">URI (must NOT start with '/').</param>
         /// <param name="httpContent">Content of the PUT message.</param>
         /// <returns>HTTP response.</returns>
-        public Task<HttpResponseMessage?> PutAsync(string uri, HttpContent? httpContent = null);
+        public Task<HttpResponseMessage> PutAsync(string uri, HttpContent? httpContent = null);
 
         /// <summary>
         /// Send a DELETE message using the base address of <see cref="Client"/>.
         /// </summary>
         /// <param name="uri">URI (must NOT start with '/').</param>
         /// <returns>HTTP response.</returns>
-        public Task<HttpResponseMessage?> DeleteAsync(string uri);
+        public Task<HttpResponseMessage> DeleteAsync(string uri);
     }
 }
