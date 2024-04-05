1179de52d0684417a66c092f47d1680294b5c39a
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2024-04-05 11:28:23 +0200

    WIP. Trying to add a AuthenticationStateProvider

---
+++ PubHub/PubHub.AdminPortal/Components/Auth/CustomAuthStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace PubHub.AdminPortal.Components.Auth
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage, IHttpClientFactory httpClientFactory)
+        {
+            _localStorage = localStorage;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var claims = await 
+        }
+    }
+}
