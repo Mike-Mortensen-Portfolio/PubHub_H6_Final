7364761bd5e739f1ad817e3420196fcc2b025b85
Sjimooon <122673993+Sjimooon@users.noreply.github.com>
2024-04-05 17:06:28 +0200

    Fix build errors.

--- PubHub/PubHub.AdminPortal/Components/Models/AccountForm.cs
+++ PubHub/PubHub.AdminPortal/Components/Models/AccountForm.cs
@@ -5,10 +5,11 @@ namespace PubHub.AdminPortal.Components.Models
     public class AccountForm
     {
         public string Email { get; set; }
+        public string Password { get; set; }
 
         public AccountCreateModel CreateAccountModel()
         {
-            return new AccountCreateModel { Email = Email };
+            return new AccountCreateModel { Email = Email, Password = Password };
         }
     }
 }
