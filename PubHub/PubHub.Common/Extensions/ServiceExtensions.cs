ebb3799451380441f0ca2a022c0c0543557beb72
Sjimooon <122673993+Sjimooon@users.noreply.github.com>
2024-04-05 17:12:43 +0200

    Use Polly pipeline.

--- PubHub/PubHub.Common/Extensions/ServiceExtensions.cs
+++ PubHub/PubHub.Common/Extensions/ServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Retry;
 using PubHub.Common.ApiService;
 using PubHub.Common.Services;
 
@@ -6,6 +9,8 @@ namespace PubHub.Common.Extensions
 {
     public static class ServiceExtensions
     {
+        private const string POLLY_PIPELINE = "polly-pipeline";
+
         public static IServiceCollection AddPubHubServices(this IServiceCollection services, Action<ApiOptions> apiOptions)
         {
             if (apiOptions == null)
@@ -26,12 +31,16 @@ namespace PubHub.Common.Extensions
             var uri = new Uri(apiOptions.Address);
             if (apiOptions.ConfigureForMobile)
             {
-                services.AddSingleton<IHttpClientService>(sp => new HttpClientService(new HttpClient() { BaseAddress = uri }));
+                services.AddSingleton<IHttpClientService>(sp => new HttpClientService(
+                    new HttpClient() { BaseAddress = uri },
+                    services.BuildServiceProvider().GetRequiredKeyedService<ResiliencePipeline>(POLLY_PIPELINE)));
             }
             else
             {
                 var clientName = apiOptions.HttpClientName ?? ApiConstants.HTTPCLIENT_NAME;
-                services.AddScoped<IHttpClientService>(sp => new HttpClientService(services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient(clientName)));
+                services.AddScoped<IHttpClientService>(sp => new HttpClientService(
+                    services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>().CreateClient(clientName),
+                    services.BuildServiceProvider().GetRequiredKeyedService<ResiliencePipeline>(POLLY_PIPELINE)));
                 services.AddHttpClient(clientName, options => { options.BaseAddress = uri; });
             }
 
@@ -42,7 +51,8 @@ namespace PubHub.Common.Extensions
                 .AddScoped<IAuthorService, AuthorService>()
                 .AddScoped<IGenreService, GenreService>()
                 .AddScoped<IAuthenticationService, AuthenticationService>()
-                .AddScoped<IContentTypeService, ContentTypeService>();
+                .AddScoped<IContentTypeService, ContentTypeService>()
+                .AddPolly();
         }
 
         //private static IServiceCollection AddScopedApiService<TService, TConcrete>(this IServiceCollection services, ApiOptions apiOptions) where TConcrete : ServiceRoot, TService where TService : class
@@ -55,5 +65,37 @@ namespace PubHub.Common.Extensions
         //        return instance!;
         //    });
         //}
+
+        private static IServiceCollection AddPolly(this IServiceCollection services)
+        {
+            services.AddResiliencePipeline(POLLY_PIPELINE, builder =>
+            {
+                builder.AddRetry(new RetryStrategyOptions()
+                {
+                    ShouldHandle = static args =>
+                    {
+                        if (args.Outcome.Result is HttpResponseMessage responseMessage &&
+                            responseMessage.IsSuccessStatusCode)
+                        {
+                            // Request succeeded.
+                            return new ValueTask<bool>(false);
+                        }
+
+                        // Request failed.
+                        return new ValueTask<bool>(true);
+                    },
+                    MaxRetryAttempts = 5,
+                    DelayGenerator = static args => new ValueTask<TimeSpan?>(TimeSpan.FromSeconds(0.2 * Math.Pow(2, args.AttemptNumber))),
+                    OnRetry = static args =>
+                    {
+                        Debug.WriteLine($"Polly: Retrying in {args.RetryDelay} ...");
+
+                        return default;
+                    }
+                });
+            });
+
+            return services;
+        }
     }    
 }
