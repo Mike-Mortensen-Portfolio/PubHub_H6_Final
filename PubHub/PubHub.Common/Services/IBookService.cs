483def13d50bf3b73c740d68963e30501d0c1aa0
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2024-04-13 12:37:59 +0200

    Quick service fix after the new ServiceResult handlers.

--- PubHub/PubHub.Common/Services/IBookService.cs
+++ PubHub/PubHub.Common/Services/IBookService.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PubHub.Common.Models.Books;
 
 namespace PubHub.Common.Services
@@ -10,6 +10,6 @@ namespace PubHub.Common.Services
         Task<HttpServiceResult<BookInfoModel>> AddBookAsync(BookCreateModel bookCreateModel);
         Task<HttpServiceResult<BookInfoModel>> UpdateBookAsync(Guid bookId, BookUpdateModel bookUpdateModel);
         Task<HttpServiceResult> DeleteBookAsync(Guid bookId);
-        Task<ServiceResult> PurchaseBookAsync(Guid id);
+        Task<HttpServiceResult> PurchaseBookAsync(Guid id);
     }
 }
