4fb4b5d93b23d0215e1c8efb46df0fa0dfded87f
Jasmin Bidstrup Nielsen <jasm3956@elevcampus.dk>
2024-04-13 23:23:05 +0200

    Button

--- PubHub/PubHub.Common/ErrorSpecifications/ConflictError.cs
+++ PubHub/PubHub.Common/ErrorSpecifications/ConflictError.cs
@@ -4,5 +4,6 @@
     {
         public const string TITLE = "Conflicting values";
         public const string ERROR_MESSAGE = $"We couldn't processs your action. Try again or contact PubHub support if the problem persists\nError: {ErrorsCodeConstants.INVALID_TOKEN}";
+        public const string BUTTON_TEXT = "OK";
     }
 }
