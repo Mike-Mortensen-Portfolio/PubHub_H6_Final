b41bc2c8917be5df884f3e59a8c5655bd4cb1983
Jasmin Nielsen <61870713+jeongoks@users.noreply.github.com>
2024-04-18 09:06:32 +0200

    Update ValidationAnnotationConstants.cs

--- PubHub/PubHub.Common/ValidationAnnotationConstants.cs
+++ PubHub/PubHub.Common/ValidationAnnotationConstants.cs
@@ -5,7 +5,7 @@
         public const int MAX_NAME_LENGTH = 128;
         public const int MAX_EMAIL_LENGTH = 256;
         public const int MAX_PASSWORD_LENGTH = 64;
-        public const int MAX_TITLE_LENGTH = 150;
-        public const int MAX_SUMMARY_LENGTH = 2000;
+        public const int MAX_TITLE_LENGTH = 256;
+        public const int MAX_SUMMARY_LENGTH = 1024;
     }
 }
