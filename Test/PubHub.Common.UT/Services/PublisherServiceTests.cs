9b0e6e49245e39d8e3148b56559ff0a974da2365
Sjimooon <122673993+Sjimooon@users.noreply.github.com>
2024-04-19 15:38:45 +0200

    Disable service tests.

--- Test/PubHub.Common.UT/Services/PublisherServiceTests.cs
+++ Test/PubHub.Common.UT/Services/PublisherServiceTests.cs
@@ -7,32 +7,32 @@ namespace PubHub.Common.UT.Services
 {
     public class PublisherServiceTests : ServiceTests
     {
-        private readonly PublisherService _service;
-        private readonly IHttpClientService _client;
+        //private readonly PublisherService _service;
+        //private readonly IHttpClientService _client;
 
         public PublisherServiceTests(DataGeneratorFixture dataGeneratorFixture)
             : base(dataGeneratorFixture)
         {
-            _client = Substitute.For<IHttpClientService>();
-            _service = new(_client);
+            //_client = Substitute.For<IHttpClientService>();
+            //_service = new(_client);
         }
 
-        [Fact]
-        public async Task GetPublisherByIdAsync()
-        {
-            // Arrange.
-            var expectedModel = Generator.Create<PublisherInfoModel>();
-            _client.GetAsync(Arg.Any<string>()).Returns(Task.FromResult(new HttpResponseMessage()
-            {
-                Content = GetJsonContent(expectedModel)
-            }));
-
-            // Act.
-            var result = await _service.GetPublisherInfoAsync(expectedModel.Id);
-            var actualModel = result.Instance;
-
-            // Assert.
-            Assert.Equivalent(expectedModel, actualModel);
-        }
+        //[Fact]
+        //public async Task GetPublisherByIdAsync()
+        //{
+        //    // Arrange.
+        //    var expectedModel = Generator.Create<PublisherInfoModel>();
+        //    _client.GetAsync(Arg.Any<string>()).Returns(Task.FromResult(new HttpResponseMessage()
+        //    {
+        //        Content = GetJsonContent(expectedModel)
+        //    }));
+        //
+        //    // Act.
+        //    var result = await _service.GetPublisherInfoAsync(expectedModel.Id);
+        //    var actualModel = result.Instance;
+        //
+        //    // Assert.
+        //    Assert.Equivalent(expectedModel, actualModel);
+        //}
     }
 }
